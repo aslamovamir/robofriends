ad5e58705778c13087cfcb5a24b25573e3338040
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-12-20 22:06:55 -0500

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email: 'leanervin@lasting.rv'
+	},
+	{
+		id: 3,
+		name: 'Patricia Bauch',
+		username: 'Clement',
+		email: 'patriuch@clement.org'
+	},
+	{
+		id: 4,
+		name: 'Roger Heiz',
+		username: 'Krick',
+		email: 'roghez@krck.org'
+	},
+	{
+		id: 5,
+		name: 'Emily Rondolf',
+		username: 'Jaeizz',
+		email: 'rondemily@jez.zz'
+	},
+	{
+		id: 6,
+		name: 'Ted Heitkins',
+		username: 'Meteor',
+		email: 'ted@Meteor.org'
+	},
+	{
+		id: 7,
+		name: 'Larisa Joshin',
+		username: 'Krotud',
+		email: 'larisaj@krot.gp'
+	},
+	{
+		id: 8,
+		name: 'Larina Leudowski',
+		username: 'Reimen',
+		email: 'larleud@rmn.org'
+	},
+	{
+		id: 9,
+		name: 'Krista Jey',
+		username: 'Kontor',
+		email: 'krishey@kontor.io'
+	},
+	{
+		id: 10,
+		name: 'Trodski Chelkovich',
+		username: 'Volkova',
+		email: 'trodskichelvich@volkova.ru'
+	}
+];
